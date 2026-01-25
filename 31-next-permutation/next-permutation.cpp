b7eb0c4813fcf42ffc208187b03a2b5d3f6d76f3
Shrijal Singh <150268923+Shrijalsingh6393@users.noreply.github.com>
2026-01-25 21:55:21 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (32.21%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -4,12 +4,10 @@ public:
         int n = nums.size();
         int i = n - 2;
 
-        // 1️⃣ find pivot
         while (i >= 0 && nums[i] >= nums[i + 1]) {
             i--;
         }
 
-        // 2️⃣ swap with next greater element
         if (i >= 0) {
             int j = n - 1;
             while (nums[j] <= nums[i]) {
@@ -18,7 +16,6 @@ public:
             swap(nums[i], nums[j]);
         }
 
-        // 3️⃣ reverse the suffix
         reverse(nums.begin() + i + 1, nums.end());
     }
 };
