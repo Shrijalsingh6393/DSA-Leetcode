4e08a0019d471cf4b975143b29028781a6fa59ee
Shrijal Singh <150268923+Shrijalsingh6393@users.noreply.github.com>
2026-01-27 20:00:09 +0530

    Time: 21 ms (5.61%) | Memory: 49.1 MB (5.66%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -6,20 +6,17 @@ public:
 
         string ans = "";
 
-        for (int i = 0; i < n; i++) {
-            if (s[i] == ' ') continue;   // skip spaces
-
+        for(int i=0;i<n;i++){
             string word = "";
-            while (i < n && s[i] != ' ') {
-                word += s[i];
+            while(i<n && s[i]!=' '){
+                word = word + s[i];
                 i++;
             }
-
-            reverse(word.begin(), word.end());
-
-            if (!ans.empty()) ans += " ";
-            ans += word;
+            reverse(word.begin(),word.end());
+            if(word.length()>0){
+                ans = ans + " " + word;
+            }
         }
-        return ans;
+        return ans.substr(1);
     }
 };
