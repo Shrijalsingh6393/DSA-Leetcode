75f74694b79ea674b545dffe4d385b504bd3fceb
Shrijal Singh <150268923+Shrijalsingh6393@users.noreply.github.com>
2026-01-28 11:11:50 +0530

    Time: 171 ms (85.78%) | Memory: 110.6 MB (68.89%) - LeetSync

--- 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.cpp
+++ 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.cpp
@@ -1,28 +1,43 @@
 class Solution {
 public:
     int countDistinctIntegers(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> arr(nums.begin(),nums.end());
+        // int n = nums.size();
+        // vector<int> arr(nums.begin(),nums.end());
+        // unordered_set<int> s;
+
+        // for(int i=0;i<n;i++){
+        //     int rev = 0;
+        //     while(arr[i]!=0){
+        //         int ld = arr[i]%10;
+        //         rev = rev*10 + ld;
+        //         arr[i] = arr[i]/10;
+        //     }
+        //     nums.push_back(rev);
+        // }
+
+        // int count = 0;
+
+        // for(int i=0;i<nums.size();i++){
+        //     if(s.find(nums[i])==s.end()){
+        //         s.insert(nums[i]);
+        //         count++;
+        //     }
+        // }
+        // return count;
+
         unordered_set<int> s;
 
-        for(int i=0;i<n;i++){
-            int rev = 0;
-            while(arr[i]!=0){
-                int ld = arr[i]%10;
-                rev = rev*10 + ld;
-                arr[i] = arr[i]/10;
+        for (int x : nums) {
+            s.insert(x);
+
+            int rev = 0, temp = x;
+            while (temp != 0) {
+                rev = rev * 10 + (temp % 10);
+                temp /= 10;
             }
-            nums.push_back(rev);
+            s.insert(rev);
         }
 
-        int count = 0;
-
-        for(int i=0;i<nums.size();i++){
-            if(s.find(nums[i])==s.end()){
-                s.insert(nums[i]);
-                count++;
-            }
-        }
-        return count;
+        return s.size();
     }
 };
