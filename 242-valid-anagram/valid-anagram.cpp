3c595ac54410d94c2e44dd4bbf7c6b9ce42db644
Shrijal Singh <150268923+Shrijalsingh6393@users.noreply.github.com>
2026-01-28 14:51:50 +0530

    Time: 3 ms (47.12%) | Memory: 9.7 MB (47.98%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,29 +1,51 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int>m1;
-        unordered_map<char,int>m2;
+        // unordered_map<char,int>m1;
+        // unordered_map<char,int>m2;
+
+        // if(s.length() != t.length()){
+        //     return false;
+        // }
+
+
+        // for(int i=0;i<s.length();i++){
+        //     m1[s[i]]++;
+        // }
+
+        // for(int i=0;i<t.length();i++){
+        //     m2[t[i]]++;
+        // }
+
+        
+        // for(auto el : m1){
+        //     if(m2[el.first]!=el.second){
+        //         return false;
+        //     }
+        // }
+
+        // return true;
+
+
+        unordered_map<char,int>m;
 
         if(s.length() != t.length()){
             return false;
         }
 
-
-        for(int i=0;i<s.length();i++){
-            m1[s[i]]++;
+        for(auto e : s){
+            m[e]++;
         }
 
-        for(int i=0;i<t.length();i++){
-            m2[t[i]]++;
+        for(auto e : t){
+            m[e]--;
         }
 
-        
-        for(auto el : m1){
-            if(m2[el.first]!=el.second){
+        for(auto e : m){
+            if(e.second<0){
                 return false;
             }
         }
-
         return true;
     }
 };
