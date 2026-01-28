075fc1260cb056dfc4d9eb156ba85246c845cd93
Shrijal Singh <150268923+Shrijalsingh6393@users.noreply.github.com>
2026-01-28 11:12:45 +0530

    Time: 192 ms (35.47%) | Memory: 118.5 MB (8.30%) - LeetSync

--- 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.cpp
+++ 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.cpp
@@ -1,43 +1,45 @@
 class Solution {
 public:
     int countDistinctIntegers(vector<int>& nums) {
-        // int n = nums.size();
-        // vector<int> arr(nums.begin(),nums.end());
-        // unordered_set<int> s;
-
-        // for(int i=0;i<n;i++){
-        //     int rev = 0;
-        //     while(arr[i]!=0){
-        //         int ld = arr[i]%10;
-        //         rev = rev*10 + ld;
-        //         arr[i] = arr[i]/10;
-        //     }
-        //     nums.push_back(rev);
-        // }
-
-        // int count = 0;
-
-        // for(int i=0;i<nums.size();i++){
-        //     if(s.find(nums[i])==s.end()){
-        //         s.insert(nums[i]);
-        //         count++;
-        //     }
-        // }
-        // return count;
-
+        int n = nums.size();
+        vector<int> arr(nums.begin(),nums.end());
         unordered_set<int> s;
 
-        for (int x : nums) {
-            s.insert(x);
-
-            int rev = 0, temp = x;
-            while (temp != 0) {
-                rev = rev * 10 + (temp % 10);
-                temp /= 10;
+        for(int i=0;i<n;i++){
+            int rev = 0;
+            while(arr[i]!=0){
+                int ld = arr[i]%10;
+                rev = rev*10 + ld;
+                arr[i] = arr[i]/10;
             }
-            s.insert(rev);
+            nums.push_back(rev);
         }
 
-        return s.size();
+        int count = 0;
+
+        for(int i=0;i<nums.size();i++){
+            if(s.find(nums[i])==s.end()){
+                s.insert(nums[i]);
+                count++;
+            }
+        }
+        return count;
+
+
+
+        // unordered_set<int> s;
+
+        // for (int x : nums) {
+        //     s.insert(x);
+
+        //     int rev = 0, temp = x;
+        //     while (temp != 0) {
+        //         rev = rev * 10 + (temp % 10);
+        //         temp /= 10;
+        //     }
+        //     s.insert(rev);
+        // }
+
+        // return s.size();
     }
 };
