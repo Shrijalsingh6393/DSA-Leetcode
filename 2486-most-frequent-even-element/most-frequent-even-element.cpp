037547c9ca9d2098e5da81ef492d3171d01384f4
Shrijal Singh <150268923+Shrijalsingh6393@users.noreply.github.com>
2026-02-01 15:55:07 +0530

    Time: 33 ms (34.70%) | Memory: 45.1 MB (21.95%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.cpp
+++ 2486-most-frequent-even-element/most-frequent-even-element.cpp
@@ -6,23 +6,53 @@ public:
         unordered_map<int,int>m;
         
         for(int i=0;i<n;i++){
-            m[nums[i]]++;
-        }
-
-        int maxf = 0; 
-        int ans = -1;
-        for(auto el : m){
-            int val = el.first;
-            int freq = el.second;
-
-            if(val % 2 == 0) {
-                if(freq > maxf || (freq == maxf && val < ans)) {
-                    maxf = freq;
-                    ans = val;
-                }
+            if(nums[i]%2==0){
+                m[nums[i]]++;
             }
         }
 
-        return ans;
+        if(m.empty()) return -1;
+
+        vector<pair<int,int>>p;
+        vector<int>ans;
+
+        for(auto el : m){
+            p.push_back(el);
+        }
+
+        sort(p.begin(),p.end());
+
+        int maxc = 0;
+        for(int i=0;i<p.size();i++){
+        maxc = max(maxc,p[i].second);
+        }
+
+        for(int i=0;i<p.size();i++){
+            if(p[i].second==maxc){
+            ans.push_back(p[i].first);
+            }
+        }
+
+        int minA = ans[0];
+        for(int i=0;i<ans.size();i++){
+            minA = min(minA,ans[i]);
+        }
+
+        return minA;
+        // int maxf = 0; 
+        // int ans = -1;
+        // for(auto el : m){
+        //     int val = el.first;
+        //     int freq = el.second;
+
+        //     if(val % 2 == 0) {
+        //         if(freq > maxf || (freq == maxf && val < ans)) {
+        //             maxf = freq;
+        //             ans = val;
+        //         }
+        //     }
+        // }
+
+        // return ans;
     }
 };
