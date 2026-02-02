d882248d420b01e983f6b2eb775d2bb50fa7bbc5
Shrijal Singh <150268923+Shrijalsingh6393@users.noreply.github.com>
2026-02-02 21:44:23 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (5.64%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -3,16 +3,13 @@ public:
     bool isValid(string str) {
         stack<char>st;
 
-        for(int i = 0; i < str.length(); i++) {
-            if(str[i] == '(' || str[i] == '{' || str[i] == '[') {
-                st.push(str[i]);
-            } 
-            else {
-                if(st.empty()) return false;
-                if(str[i] == ')' && st.top() != '(') return false;
-                if(str[i] == '}' && st.top() != '{') return false;
-                if(str[i] == ']' && st.top() != '[') return false;
-                st.pop();
+        for(char c : str) {
+        if(c == '(') st.push(')');
+        else if(c == '{') st.push('}');
+        else if(c == '[') st.push(']');
+        else {
+            if(st.empty() || st.top() != c) return false;
+            st.pop();
             }
         }
 
