ba3ea4fe086911eea481a673ddecc2589050b909
Shrijal Singh <150268923+Shrijalsingh6393@users.noreply.github.com>
2026-02-04 19:37:49 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (33.45%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
@@ -11,39 +11,51 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        if(head == NULL) return head;
+        // if(head == NULL) return head;
+
+        // ListNode* temp = head;
+        // vector<int>a;
+
+        // while(temp!=NULL){
+        //     a.push_back(temp->val);
+        //     temp=temp->next;
+        // }
+
+        // vector<int>b;
+        // b.push_back(a[0]);
+
+        // for(int i=1;i<a.size();i++){
+        //     if(a[i]!=a[i-1]){
+        //         b.push_back(a[i]);
+        //     }
+        // }
+
+        // temp = head;
+        // int i = 0;
+        // ListNode* prev = NULL; 
+
+        // while(temp!=NULL && i<b.size()){
+        //     temp->val = b[i++];
+        //     prev = temp;
+        //     temp = temp->next;
+        // }
+
+        // if(prev != NULL) {
+        //     prev->next = NULL;
+        // }
+
+        // return head;
 
         ListNode* temp = head;
-        vector<int>a;
 
-        while(temp!=NULL){
-            a.push_back(temp->val);
-            temp=temp->next;
-        }
-
-        vector<int>b;
-        b.push_back(a[0]);
-
-        for(int i=1;i<a.size();i++){
-            if(a[i]!=a[i-1]){
-                b.push_back(a[i]);
+        while(temp!=NULL && temp->next != NULL){
+            if(temp->val == temp->next->val){
+               temp->next = temp->next->next;
+            }
+            else{
+                temp = temp->next; 
             }
         }
-
-        temp = head;
-        int i = 0;
-        ListNode* prev = NULL; 
-
-        while(temp!=NULL && i<b.size()){
-            temp->val = b[i++];
-            prev = temp;
-            temp = temp->next;
-        }
-
-        if(prev != NULL) {
-            prev->next = NULL;
-        }
-
         return head;
     }
 };
