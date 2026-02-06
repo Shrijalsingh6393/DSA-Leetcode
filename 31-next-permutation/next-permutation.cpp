18fdad5567ed22c74f440b17ccefa504aa3779f6
Shrijal Singh <150268923+Shrijalsingh6393@users.noreply.github.com>
2026-02-06 15:38:16 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (91.78%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
-        int i = n-2;
+        int i=n-2;
 
         while(i>=0 && nums[i]>=nums[i+1]){
             i--;
-        } 
+        }
 
         if(i>=0){
             int j=n-1;
@@ -15,6 +15,7 @@ public:
             }
             swap(nums[i],nums[j]);
         }
+
         reverse(nums.begin()+i+1,nums.end());
     }
 };
