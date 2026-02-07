1cca81e485e8bad2800ed10dc907b0d26936450c
Shrijal Singh <150268923+Shrijalsingh6393@users.noreply.github.com>
2026-02-07 19:41:07 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (44.30%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    string func(string s){
-        int i = 0;
-        while(i < s.size() && s[i] == ' ') i++;
-        s = s.substr(i);
-
-        int pos = s.find(' ');
-        if(pos==string::npos){return s;}
-
-        string first = s.substr(0,pos);
-        string rest = s.substr(pos+1);
-
-        string res = func(rest);
-
-        if(res.size() == 0) return first;
-        return res + " " + first;
-    }
     string reverseWords(string s) {
-        return func(s);
+        int n = s.length();
+        reverse(s.begin(),s.end());
+
+        string ans = "";
+
+        for(int i=0;i<n;i++){
+            string word = "";
+            while(i<n && s[i]!=' '){
+                word += s[i]; 
+                i++;
+            }
+            reverse(word.begin(),word.end());
+
+            if(word.length()>0){
+                ans += " " + word;
+            }
+        }
+        return ans.substr(1);
     }
 };
