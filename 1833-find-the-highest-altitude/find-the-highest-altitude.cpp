b5ef25d168b2c5a8a0bca3da8603be77ffa4c22e
Shrijal Singh <150268923+Shrijalsingh6393@users.noreply.github.com>
2026-02-07 20:07:09 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (45.17%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
@@ -3,15 +3,17 @@ public:
     int largestAltitude(vector<int>& gain) {
         int n = gain.size();
         vector<int>v;
-
         v.push_back(0);
+
         for(int i=0;i<n;i++){
             v.push_back(v[i]+gain[i]);
         }
 
         int maxel = v[0];
         for(int i=0;i<v.size();i++){
-            maxel = max(maxel,v[i]);
+            if(v[i]>maxel){
+                maxel = v[i];
+            }
         }
         return maxel;
     }
