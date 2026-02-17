3d1596977ca219181a43861763937e7e4d647dcb
Shrijal Singh <150268923+Shrijalsingh6393@users.noreply.github.com>
2026-02-17 09:38:11 +0530

    Time: 84 ms (18.13%) | Memory: 88.1 MB (18.63%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
     vector<vector<string>>ans;
-
-    bool isPalindrome(string &s, int l, int r) {
-        while(l < r) {
-            if(s[l] != s[r]) return false;
-            l++;
-            r--;
+    bool isPalindrome(string s,int st,int e){
+        while(st<e){
+            if(s[st]!=s[e]){return false;}
+            st++;
+            e--;
         }
         return true;
     }
-
-    void func(string &s,int index,vector<string>&v){
+    void func(string s,vector<string>&v,int index){
         if(index==s.size()){
             ans.push_back(v);
             return;
         }
-
         for(int i=index;i<s.size();i++){
             if(isPalindrome(s,index,i)){
-            v.push_back(s.substr(index, i - index + 1));
-            func(s,i+1,v);
-            v.pop_back();
+                v.push_back(s.substr(index,i-index+1));
+                func(s,v,i+1);
+                v.pop_back();
             }
         }
     }
-
     vector<vector<string>> partition(string s) {
         vector<string>v;
-        func(s,0,v);
-
+        func(s,v,0);
         return ans;
     }
 };
