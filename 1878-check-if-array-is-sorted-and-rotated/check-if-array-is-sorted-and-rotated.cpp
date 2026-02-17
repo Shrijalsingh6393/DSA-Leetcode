32fc6af2fff324a6de8bdabccdfb838cf55ac747
Shrijal Singh <150268923+Shrijalsingh6393@users.noreply.github.com>
2026-02-17 20:23:02 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (6.41%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -4,11 +4,15 @@ public:
         int n = nums.size();
         vector<int>b(n);
         int pos = 0;
+        int count=0;
         for(int i=0;i<n-1;i++){
             if(nums[i]>nums[i+1]){
+                count++;
                 pos = i+1;
             }
         }
+
+        if(count>1){return false;}
 
         for(int i=0;i<n;i++){
             b[i] = nums[(i+pos)%n];
