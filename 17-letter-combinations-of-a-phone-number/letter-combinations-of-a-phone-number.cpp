0d29555b49409e833cbe6148d1cf0d81a69a6f79
Shrijal Singh <150268923+Shrijalsingh6393@users.noreply.github.com>
2026-02-17 09:18:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (18.60%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    vector<string> ans;
-    void solve(string &d, int i, string s, vector<string> &v) {
-        if(i == d.size()) {
+    vector<string>ans;
+    void func(string digits,vector<string>&v,int index,string s){
+        if(index==digits.length()){
             ans.push_back(s);
             return;
         }
-
-        string letters = v[d[i] - '0'];
-
-        for(int j = 0; j < letters.size(); j++) {
-            solve(d, i + 1, s + letters[j], v);
+        string letters = v[digits[index]-'0'];
+        for(int i=0;i<letters.size();i++){
+            func(digits,v,index+1,s+letters[i]);
         }
     }
-
     vector<string> letterCombinations(string digits) {
-       if(digits.size() == 0) return {};
-
-        vector<string> v =
-        {"", "", "abc", "def", "ghi",
-         "jkl", "mno", "pqrs", "tuv", "wxyz"};
-
-        solve(digits, 0, "", v);
-
-        return ans; 
+        if(digits.size()==0){
+            return {};
+        }
+        vector<string>v = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        func(digits,v,0,"");
+        return ans;
     }
 };
