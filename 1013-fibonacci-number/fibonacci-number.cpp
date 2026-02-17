4d3163087151e8ec98bf7354fb596a44614646be
Shrijal Singh <150268923+Shrijalsingh6393@users.noreply.github.com>
2026-02-17 10:04:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (15.74%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,12 +1,34 @@
 class Solution {
 public:
-    int fib(int n) {
+    int func(vector<int>&dp,int n){
         if(n==0 || n==1){
-            return n;
+            return dp[n]=n;
         }
-        int x = fib(n-1);
-        int y = fib(n-2);
+        // int x;
+        // if(dp[n-1]!=-1){x=dp[n-1];}
+        // else{x = func(dp,n-1);}
+        // int y;
+        // if(dp[n-2]!=-1){y = dp[n-2];}
+        // else{y = func(dp,n-2);}
 
-        return x+y;
+        if(dp[n]!=-1){return dp[n];}
+        int x = func(dp,n-1);
+        int y = func(dp,n-2);
+
+        return dp[n]=x+y;
+    }
+
+    int fib(int n) {
+        // if(n==0 || n==1){
+        //     return n;
+        // }
+        // int x = fib(n-1);
+        // int y = fib(n-2);
+
+        // return x+y;
+
+        vector<int>dp(n+1,-1);
+
+        return func(dp,n);
     }
 };
