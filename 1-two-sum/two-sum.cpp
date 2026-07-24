e7cf75399c28ed8cfb0442190a8f00d3e65f1f02
Shrijal Singh <150268923+Shrijalsingh6393@users.noreply.github.com>
2026-07-24 10:47:04 +0530

    Time: 1 ms (76.80%) | Memory: 15 MB (20.88%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -14,30 +14,47 @@ public:
         // return {};
 
 
-        //2nd
+        //2. 2-pointer
+
+        // int n = nums.size();
+        // vector<pair<int,int>>v;
+
+        // for(int i=0;i<n;i++){
+        //     v.push_back({nums[i],i});
+        // }
+        // sort(v.begin(),v.end());
+        // int s = 0;
+        // int e = n-1;
+
+        // while(s<e){
+        //     int sum = v[s].first+v[e].first;
+        //     if(sum==target){
+        //         return {v[s].second,v[e].second};
+        //     }
+        //     else if(sum>target){
+        //         e--;
+        //     }
+        //     else{
+        //         s++;
+        //     }
+        // }
+        // return {};
+
+
+
+        //3. map
 
         int n = nums.size();
-        vector<pair<int,int>>v;
+        unordered_map<int,int>mp;
 
         for(int i=0;i<n;i++){
-            v.push_back({nums[i],i});
+            int a = target - nums[i];
+            if(mp.find(a)!=mp.end()){
+                return {i,mp[a]};
+            }
+            mp[nums[i]] = i;
         }
-        sort(v.begin(),v.end());
-        int s = 0;
-        int e = n-1;
 
-        while(s<e){
-            int sum = v[s].first+v[e].first;
-            if(sum==target){
-                return {v[s].second,v[e].second};
-            }
-            else if(sum>target){
-                e--;
-            }
-            else{
-                s++;
-            }
-        }
         return {};
     }
 };
