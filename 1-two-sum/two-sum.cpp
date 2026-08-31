edb45e664dedc6b3716067d49d5c3692b8bf8924
Shrijal Singh <150268923+Shrijalsingh6393@users.noreply.github.com>
2026-08-31 19:20:33 +0530

    Time: 35 ms (36.56%) | Memory: 14.2 MB (74.93%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,60 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // 1st
-        // int n = nums.size();
-    
-        // for(int i=0;i<n;i++){
-        //     for(int j=i+1;j<n;j++){
-        //         if(nums[i]+nums[j]==target){
-        //             return {i,j};
-        //         }
-        //     }
-        // }
-        // return {};
-
-
-        //2. 2-pointer
-
-        // int n = nums.size();
-        // vector<pair<int,int>>v;
-
-        // for(int i=0;i<n;i++){
-        //     v.push_back({nums[i],i});
-        // }
-        // sort(v.begin(),v.end());
-        // int s = 0;
-        // int e = n-1;
-
-        // while(s<e){
-        //     int sum = v[s].first+v[e].first;
-        //     if(sum==target){
-        //         return {v[s].second,v[e].second};
-        //     }
-        //     else if(sum>target){
-        //         e--;
-        //     }
-        //     else{
-        //         s++;
-        //     }
-        // }
-        // return {};
-
-
-
-        //3. map
-
         int n = nums.size();
-        unordered_map<int,int>mp;
 
         for(int i=0;i<n;i++){
-            int a = target - nums[i];
-            if(mp.find(a)!=mp.end()){
-                return {i,mp[a]};
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){
+                    return {i,j};
+                }
             }
-            mp[nums[i]] = i;
         }
-
         return {};
     }
 };
